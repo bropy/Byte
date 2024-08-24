c1a4c1b3a96d4d5f2632e209bb48bdfc69ade898
bropy <bohdandesna22@gmail.com>
2024-08-24 15:48:20 +0300

    fix deploy errors

--- src/main/java/com/steam/demo/entity/User.java
+++ src/main/java/com/steam/demo/entity/User.java
@@ -1,18 +1,15 @@
 package com.steam.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "users")
@@ -36,32 +33,38 @@ public class User {
 
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String firstName;
+
     @Column(nullable = false)
     private String lastName;
+
     @Column(nullable = false)
     private LocalDate birthDate;
 
-    @ManyToMany(fetch= FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
-            name="user_roles",
-            joinColumns = {@JoinColumn(name="user_id",referencedColumnName = "id")},
-            inverseJoinColumns = {@JoinColumn(name="role_id",referencedColumnName = "id")}
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private List<Role> roles = new ArrayList<>();
-
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private Profile profile;
 
-
     @ManyToMany
     @JoinTable(
-        name = "friends",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "friend_id")
+            name = "friends",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
     )
+    @JsonManagedReference
     private Set<User> friends;
+
+    @ManyToMany(mappedBy = "friends")
+    @JsonBackReference
+    private Set<User> friendsOf; // To avoid cyclic references
 }
