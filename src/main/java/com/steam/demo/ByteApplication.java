7d779e13e9a055e2ec586daece6b2dd1de26507c
bropy <bohdandesna22@gmail.com>
2024-08-24 14:34:33 +0300

    fix deploy errors

--- src/main/java/com/steam/demo/ByteApplication.java
+++ src/main/java/com/steam/demo/ByteApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EntityScan("com.steam.demo.entity")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class ByteApplication {
+public class ByteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ByteApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ByteApplication.class);
 	}
 }
