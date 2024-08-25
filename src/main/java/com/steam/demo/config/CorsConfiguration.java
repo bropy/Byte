e01a9a808e63cf292a59c5e9fd77f241b6bb8c6c
bropy <bohdandesna22@gmail.com>
2024-08-25 15:26:09 +0300

    Update of CORS security

---
+++ src/main/java/com/steam/demo/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.steam.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://byteweb-9d67fdfd7b15.herokuapp.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
